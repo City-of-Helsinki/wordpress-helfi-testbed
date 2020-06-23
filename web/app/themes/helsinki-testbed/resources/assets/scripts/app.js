1e9504c10f9e7305c2d25e7d0f5f9a7b49fcb63b
Oskar Schöldström <m@oxy.fi>
2020-06-23 12:17:33 -0300

    remove fontawesome and vue

--- web/app/themes/helsinki-testbed/resources/assets/scripts/app.js
+++ web/app/themes/helsinki-testbed/resources/assets/scripts/app.js
@@ -2,18 +2,3 @@
  * External Dependencies
  */
 // import $ from 'jquery';
-
-/**
- * Internal Dependencies
- */
-import { init as foundation } from './common/foundation';
-import { init as fontawesome } from './common/fontawesome';
-import { init as accordion } from './components/accordion';
-import { init as vue } from './vue';
-
-fontawesome();
-
-vue('#app').finally(() => {
-  foundation();
-  accordion('.schema-faq', {itemSelector: '.schema-faq-section', titleSelector: '.schema-faq-question'});
-});

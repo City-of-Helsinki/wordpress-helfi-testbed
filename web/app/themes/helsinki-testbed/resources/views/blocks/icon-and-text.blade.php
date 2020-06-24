6c3d9d58241a7423e4b7aa1fb59715e50f09a9d0
Henri Tukiainen <henri@momocode.com>
2020-06-25 00:03:05 +0300

    Update Icon & Text block to work without heading

--- web/app/themes/helsinki-testbed/resources/views/blocks/icon-and-text.blade.php
+++ web/app/themes/helsinki-testbed/resources/views/blocks/icon-and-text.blade.php
@@ -1,10 +1,16 @@
 <div class="wp-block-hds-icon-text {{ esc_attr($block->classes) }}">
-    <div class="wp-block-hds-icon-text__icon has-{!! $iconName !!}-icon has-{!! $iconColor !!}-color">
-    </div>
-    <div class="wp-block-hds-icon-text__heading has-{!! $textColor !!}-color">
-        {!! wp_kses_post($heading) !!}
-    </div>
-    <div class="wp-block-hds-icon-text__body has-{!! $textColor !!}-color">
-        {!! wp_kses_post($body) !!}
-    </div>
+    @if ($iconName)
+        <div class="wp-block-hds-icon-text__icon has-{!! $iconName !!}-icon has-{!! $iconColor !!}-color">
+        </div>
+    @endif
+    @if ($heading)
+        <div class="wp-block-hds-icon-text__heading has-{!! $textColor !!}-color">
+            {!! wp_kses_post($heading) !!}
+        </div>
+    @endif
+    @if ($body)
+        <div class="wp-block-hds-icon-text__body has-{!! $textColor !!}-color">
+            {!! wp_kses_post($body) !!}
+        </div>
+    @endif
 </div>

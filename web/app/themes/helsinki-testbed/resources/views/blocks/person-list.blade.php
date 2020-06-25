cc0ea83d9d0938f0cd19ba76c6c99d026f810c65
Oskar Schöldström <m@oxy.fi>
2020-06-25 11:07:48 -0300

    add responsive cell sizes

--- web/app/themes/helsinki-testbed/resources/views/blocks/person-list.blade.php
+++ web/app/themes/helsinki-testbed/resources/views/blocks/person-list.blade.php
@@ -1,7 +1,7 @@
 <div class="{{ esc_attr($block->classes) }}">
   <div class="grid">
     @foreach ($persons as $person)
-      <div class="cell {{ $layout === 'vertical' ? 'cell--3' : 'cell--6' }}">
+      <div class="cell {{ $layout === 'vertical' ? 'cell--4 small:cell--2 medium:cell--3 ' : 'cell--6 medium:cell--4' }}">
         @includeFirst(['teasers.' . $person->post_type, 'teasers.teaser'], ['post' => $person, 'layout' => $layout])
       </div>
     @endforeach

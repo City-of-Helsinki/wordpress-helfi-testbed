8cd51b49632709a20566a1097182b459c7fe4636
Oskar Schöldström <m@oxy.fi>
2020-06-25 19:36:47 -0300

    add search results and search page

--- web/app/themes/helsinki-testbed/resources/views/partials/content-search.blade.php
+++ web/app/themes/helsinki-testbed/resources/views/partials/content-search.blade.php
@@ -1,15 +1,1 @@
-<article @php(post_class())>
-  <header class="entry-header">
-    <h2 class="entry-title">
-      <a href="{{ get_permalink() }}">
-        @php(the_title())
-      </a>
-    </h2>
-
-    @includeWhen(get_post_type() === 'post', 'partials/entry-meta')
-  </header>
-
-  <div class="entry-summary">
-    @php(the_excerpt())
-  </div>
-</article>
+<x-search-result :post="get_post()" />

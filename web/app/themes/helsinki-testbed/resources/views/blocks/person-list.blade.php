036f0c1ac8dbdca6c7436b4fa3c423df74e10f51
Henri Tukiainen <henri@momocode.com>
2020-06-28 05:18:01 +0300

    Fix person list styles

--- web/app/themes/helsinki-testbed/resources/views/blocks/person-list.blade.php
+++ web/app/themes/helsinki-testbed/resources/views/blocks/person-list.blade.php
@@ -1,7 +1,7 @@
-<div class="{{ esc_attr($block->classes) }}">
-  <div class="grid">
+<div class="{{ esc_attr($block->classes) }} layout-{{ $layout }}">
+  <div @if (count($persons) > 1) class="grid" @endif>
     @foreach ($persons as $person)
-      <div class="cell {{ $layout === 'vertical' ? 'xsmall:4 small:2 medium:4 large:3' : 'medium:4 large:6' }}">
+      <div @if (count($persons) > 1 || $layout === 'vertical') class="cell {{ $layout === 'vertical' ? 'xsmall:4 small:2 medium:4 large:3' : 'medium:4 large:6' }}" @endif>
         @includeFirst(['teasers.' . $person->post_type, 'teasers.teaser'], ['post' => $person, 'layout' => $layout])
       </div>
     @endforeach

3b6a9997dc388aa10b6e4a4d3c85b87f693313c0
Henri Tukiainen <henri@momocode.com>
2020-09-22 02:58:58 +0300

    Increase image sizes

--- web/app/themes/helsinki-testbed/app/setup.php
+++ web/app/themes/helsinki-testbed/app/setup.php
@@ -156,9 +156,9 @@ add_action('after_setup_theme', function () {
     add_theme_support('disable-custom-font-sizes');
 
     add_image_size('tiny', 50, 50, true);
-    add_image_size('wide', 1200);
-    add_image_size('xlarge', 1440);
-    add_image_size('xxlarge', 2000);
+    add_image_size('wide', 1920);
+    add_image_size('xlarge', 2560);
+    add_image_size('xxlarge', 3840);
 }, 20);
 
 /**

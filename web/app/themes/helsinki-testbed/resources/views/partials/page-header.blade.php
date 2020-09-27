448d41d10970bc6446a8c730301e2f3ca025a84e
Henri Tukiainen <henri@momocode.com>
2020-09-27 06:02:04 +0300

    Remove breadcrumbs from front page

--- web/app/themes/helsinki-testbed/resources/views/partials/page-header.blade.php
+++ web/app/themes/helsinki-testbed/resources/views/partials/page-header.blade.php
@@ -1,5 +1,5 @@
 <header class="entry-header">
-  @if (function_exists('yoast_breadcrumb'))
+  @if (!is_front_page() && function_exists('yoast_breadcrumb'))
     {{ yoast_breadcrumb( '<p id="breadcrumbs">','</p>' ) }}
   @endif
   <h1 class="entry-title">{!! $title !!}</h1>
